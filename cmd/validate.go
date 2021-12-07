1ebce619bcd57cf7ae9fe48c99f6cd15488b91b5
Matt Rutkowski <mrutkows@us.ibm.com>
2021-12-07 11:59:57 -0600

    Add schemas

--- cmd/validate.go
+++ cmd/validate.go
@@ -7,8 +7,13 @@ import (
 	"github.com/xeipuuv/gojsonschema"
 )
 
+// https://github.com/spdx/spdx-spec/blob/master/schemas/spdx-schema.json
+// https://github.com/CycloneDX/specification/blob/master/schema/bom-1.3.schema.json
+// https://github.com/CycloneDX/specification/blob/master/schema/bom-1.3-strict.schema.json
 const (
-	SCHEMA_SPDX_2_2_LOCAL = "file://schema/spdx/2.2/spdx-schema.json"
+	SCHEMA_SPDX_2_2_2_LOC             = "file://schema/spdx/2.2/spdx-schema.json"
+	SCHEMA_CYCLONEDX_1_3_LOCAL        = "file://schema/cyclonedx/1.3/bom-1.3.schema.json"
+	SCHEMA_CYCLONEDX_1_3_STRICT_LOCAL = "file://schema/cyclonedx/1.3/bom-1.3-strict.schema.json"
 )
 
 var (
@@ -44,7 +49,7 @@ func Validate(schema string, document string) (bool, error) {
 		return false, fmt.Errorf("schema [%s] not supported", schema)
 	}
 
-	loader := gojsonschema.NewReferenceLoader(SCHEMA_SPDX_2_2_LOCAL)
+	loader := gojsonschema.NewReferenceLoader(SCHEMA_SPDX_2_2_2_LOCAL)
 
 	// create a reusable schema object (to validate multiple documents)
 	_, err := gojsonschema.NewSchema(loader)
